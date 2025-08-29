ee0c29f46faa2357593a2fc47f926a78c7bb2322
inboxiqapp <inboxiqapp@gmail.com>
2025-08-29 16:21:49 -0500

    Update Settings.js

--- web/src/pages/Settings.js
+++ web/src/pages/Settings.js
@@ -7,10 +7,10 @@ export default function Settings() {
     <div className="p-4">
       <h2 className="text-2xl font-bold mb-4">Settings</h2>
       <label className="flex items-center space-x-2">
-        <input 
-          type="checkbox" 
-          checked={digestEnabled} 
-          onChange={() => setDigestEnabled(!digestEnabled)} 
+        <input
+          type="checkbox"
+          checked={digestEnabled}
+          onChange={() => setDigestEnabled(!digestEnabled)}
         />
         <span>Enable Weekly Digest</span>
       </label>
