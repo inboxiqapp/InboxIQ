d9a16ab7dc93b3b773b1ae074b19b7487d218a67
inboxiqapp <inboxiqapp@gmail.com>
2025-09-02 17:44:43 -0500

    Update auth.js

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -26,8 +26,16 @@ router.get(
   passport.authenticate("google", { failureRedirect: "/" }),
   (req, res) => {
     // Redirect to your frontend app
-    res.redirect("https://inboxiq-web.vercel.app/inbox");
+    res.redirect("https://inboxiqappweb.vercel.app/");
   }
 );
+
+// Expose a /me route for frontend
+router.get("/me", (req, res) => {
+  if (!req.user) {
+    return res.status(401).json({ error: "Not logged in" });
+  }
+  res.json({ email: req.user.email });
+});
 
 export default router;
